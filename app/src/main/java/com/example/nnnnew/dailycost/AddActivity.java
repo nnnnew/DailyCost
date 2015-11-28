452d2c397decf40fc0b552f983d2cbea0112943c
nnnnew <nnnnew@Natthawats-MacBook-Pro.local>
2015-11-29 04:15:49 +0700

    3

--- app/src/main/java/com/example/nnnnew/dailycost/AddActivity.java
+++ app/src/main/java/com/example/nnnnew/dailycost/AddActivity.java
@@ -1,22 +1,24 @@
 package com.example.nnnnew.dailycost;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
 
-    private AddExpenseFragment addExpenseFragment = new AddExpenseFragment();
-    private AddIncomeFragment addIncomeFragment = new AddIncomeFragment();
     private String statusTypeNow = "Expense";
+    private String selectedCatalog = "";
+    private String note = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        loadExpenseFragment();
 
         Button backButton = (Button) findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +30,7 @@ public class AddActivity extends AppCompatActivity {
 
         final Button expens = (Button) findViewById(R.id.expense_add_button);
         final Button income = (Button) findViewById(R.id.income_add_button);
+        final LinearLayout linearAmount=(LinearLayout)findViewById(R.id.linearAmount);
         expens.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,14 +38,20 @@ public class AddActivity extends AppCompatActivity {
                 int colorNonClick = Color.argb(255,255,255,255);
                 int textcolorClick = Color.argb(255,255,255,255);
                 int textcolorNonClick = Color.argb(255,0,0,0);
+
                 expens.setBackgroundColor(colorClick);
                 income.setBackgroundColor(colorNonClick);
                 expens.setTextColor(textcolorClick);
                 income.setTextColor(textcolorNonClick);
+                linearAmount.setBackgroundColor(colorClick);
 
-                loadExpenseFragment();
+                statusTypeNow = "Expense";
+
+
             }
         });
+
+
 
 
         income.setOnClickListener(new View.OnClickListener() {
@@ -56,42 +65,52 @@ public class AddActivity extends AppCompatActivity {
                 expens.setBackgroundColor(colorNonClick);
                 income.setTextColor(textcolorClick);
                 expens.setTextColor(textcolorNonClick);
+                linearAmount.setBackgroundColor(colorClick);
 
-                loadInfomceFragment();
-
+                statusTypeNow = "Income";
 
             }
         });
+
 
 
         Button doneButton = (Button) findViewById(R.id.done_button);
         doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Toast.makeText(AddActivity.this, statusTypeNow, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        LinearLayout catalogButton = (LinearLayout) findViewById(R.id.linearLayout5);
+        catalogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(statusTypeNow == "Expense") {
+                    Intent intent = new Intent(getApplicationContext(), ExpenseCatalogueActivity.class);
+                    startActivityForResult(intent, 0);
+                }
+                else {
+                    Intent intent = new Intent(getApplicationContext(), IncomeCatalogueActivity.class);
+                    startActivityForResult(intent, 1);
+                }
             }
         });
 
     }
 
-    private void loadExpenseFragment() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        statusTypeNow = "Expense";
-        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-        android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.fragment_add, addExpenseFragment);
-        ft.commit();
+        if(requestCode == 0) {
+            selectedCatalog = data.getStringExtra("catalogue");
+            note = data.getStringExtra("note");
+        }
+        else {
+            selectedCatalog = data.getStringExtra("catalogue");
+            note = data.getStringExtra("note");
+        }
+
     }
-
-    private void loadInfomceFragment() {
-
-        statusTypeNow = "Income";
-        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-        android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.fragment_add, addIncomeFragment);
-        ft.commit();
-    }
-
-
-
 }
