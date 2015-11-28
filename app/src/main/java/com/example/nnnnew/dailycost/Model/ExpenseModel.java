1b49517919b991c963eb15e0e18ba317bc085df3
nnnnew <nnnnew@Natthawats-MacBook-Pro.local>
2015-11-29 04:48:54 +0700

    4

---
+++ app/src/main/java/com/example/nnnnew/dailycost/Model/ExpenseModel.java
@@ -0,0 +1,12 @@
+package com.example.nnnnew.dailycost.Model;
+
+/**
+ * Created by nnnnew on 11/29/2015 AD.
+ */
+public class ExpenseModel {
+
+    String[] type = {"Breakfast", "Lunch", "Dinner", "Snack", "Shopping", "Movie", "Music", "Game", "Bill", "Visa", "Other"};
+
+    String[] idIcon = {"on_add_breakfast", "on_add_lunch", "on_add_dinner", "on_add_snack", "on_shopping",
+            "on_add_movie", "on_add_music", "on_add_game", "on_add_bill", "on_add_visa", "on_add_other"};
+}
